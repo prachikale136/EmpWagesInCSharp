6b86a0d1c1f0c3c243665070993e7261c3414314
Prachi <prachikale136@gmail.com>
2023-02-17 00:27:50 +0530

    calculate wages till number of days

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -4,17 +4,19 @@ namespace EmployeeWages
 {
     internal class Program
     {
+        const int IS_PART_TIME = 1;
+        const int IS_FULL_TIME = 2;
+        const int EMP_RATE_PER_HOUR = 20;
+        const int NUM_OF_WORKING_DAY = 20;
+        const int MAX_HRS_PER_MONTH = 10;
         static void Main(string[] args)
         {
-            const int IS_PART_TIME = 1;
-            const int IS_FULL_TIME = 2;
-            const int EMP_RATE_PER_HOUR = 20;
-            const int NUM_OF_WORKING_DAY = 20;
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
-            for (int days = 0; days < NUM_OF_WORKING_DAY; days++)
+            int totalEmpHrs = 0;
+            int totalWorkingDay = 0;
+            while(totalEmpHrs <= MAX_HRS_PER_MONTH && totalWorkingDay < NUM_OF_WORKING_DAY )
             {
+                totalWorkingDay++;
                 Random random = new Random();
 
                 int empCheck = random.Next(0, 3);
@@ -31,12 +33,10 @@ namespace EmployeeWages
                         break;
 
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp wage :" + empWage);
-
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days:" +totalWorkingDay + " emp Hrs:" +empHrs);
             }
-
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("total emp wages:" + totalEmpWage);
         }
         
