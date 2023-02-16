94d5a6947932b6415bf683d87869407f21f4e81c
Prachi <prachikale136@gmail.com>
2023-02-16 23:49:52 +0530

    Add Part Time Employee

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -1,23 +1,30 @@
-﻿namespace EmployeeWages
+﻿using System.ComponentModel.Design;
+
+namespace EmployeeWages
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
 
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME) 
             {
                 empHrs = 8;
             }
             else
             {
-                empHrs = 4; ;
+                empHrs = 0; ;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp wage :" +empWage);
