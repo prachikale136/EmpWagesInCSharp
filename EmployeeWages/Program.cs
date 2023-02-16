a15be55673b733dbeb7a940af479afa625391e35
Prachi <prachikale136@gmail.com>
2023-02-17 00:13:21 +0530

    calculate wages for a month

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -9,26 +9,36 @@ namespace EmployeeWages
             const int IS_PART_TIME = 1;
             const int IS_FULL_TIME = 2;
             const int EMP_RATE_PER_HOUR = 20;
+            const int NUM_OF_WORKING_DAY = 20;
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
+            int totalEmpWage = 0;
+            for (int days = 0; days < NUM_OF_WORKING_DAY; days++)
+            {
+                Random random = new Random();
 
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
-            { 
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default: 
-                    empHrs = 0;
-                    break;
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp wage :" + empWage);
 
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp wage :" +empWage);
+
+            Console.WriteLine("total emp wages:" + totalEmpWage);
         }
+        
     }
 }
