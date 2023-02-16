cb6c83ee7b276ce3f097570aa017ad5a4138c2cf
Prachi <prachikale136@gmail.com>
2023-02-16 18:02:05 +0530

    welcome Message

---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Computation");
+        }
+    }
+}
