1f1195b3079db3e60736aac4216d380a66996150
Prachi <prachikale136@gmail.com>
2023-02-16 22:17:41 +0530

    Check Emp Present or not

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -5,22 +5,20 @@
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
-            int empHrs = 0;
-            int empWage = 0;
+            //int EMP_RATE_PER_HOUR = 20;
             Random random = new Random();
 
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                empHrs = 8;
+                Console.WriteLine("Employee id present"); ;
             }
             else
             {
-                empHrs = 0;
+                Console.WriteLine("Employee is abscent");
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp wage :" +empWage);
+            /*empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp wage :" +empWage);*/
         }
     }
 }
