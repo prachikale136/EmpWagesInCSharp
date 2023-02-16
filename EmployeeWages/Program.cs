760c9f29629c33366cae91a9cbb63d60cd78093b
Prachi <prachikale136@gmail.com>
2023-02-16 23:42:07 +0530

    Daily Employee Wages

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -5,20 +5,22 @@
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
-            //int EMP_RATE_PER_HOUR = 20;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
 
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee id present"); ;
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is abscent");
+                empHrs = 4; ;
             }
-            /*empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp wage :" +empWage);*/
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp wage :" +empWage);
         }
     }
 }
